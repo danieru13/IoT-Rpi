a57f51f081bdf5447169b059f6115501d257ef37
Daniel Vanegas <danieru1313@gmail.com>
2019-09-09 20:03:08 -0500

    Fix punto2

--- punto2.py
+++ punto2.py
@@ -1,19 +1,14 @@
-####import RPi.GPIO as GPIO
 from gpiozero import Button
 import time
-
-GPIO.setmode(GPIO.BCM)
 
 # ISR
 def my_callback():
   print('You pressed the button')
 
-####GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 btn = Button(18)
 
 # Asociando la funcion handler al evento de interrupcion 
 # (cambio en estado del boton (de alto a bajo) presionado)
-####GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback)
 btn.when_pressed = my_callback
 
 try:
@@ -24,5 +19,4 @@ try:
     time.sleep(1)
 finally:
   print("Cleaning Up!")
-  exit(0)  
-  ####GPIO.cleanup()
+  exit(0)
