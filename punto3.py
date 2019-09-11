37edf9b690055e2ce78adfffd677bb71b4a0a56c
Daniel Vanegas <danieru1313@gmail.com>
2019-09-11 12:35:50 -0500

    Update punto3

--- punto3.py
+++ punto3.py
@@ -1,15 +1,27 @@
-from gpiozero import Button
+from gpiozero import Button, LED
 from picamera import PiCamera
 from datetime import datetime
+from time import sleep
 from signal import pause
 
 button = Button(2)
+led = LED(23)
 camera = PiCamera()
 
+
 def capture():
+    i = 1
+    while i<6:
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
+        i+=1
     timestamp = datetime.now().isoformat()
-    camera.capture('/home/pi/%s.jpg' % timestamp)
+    camera.capture('/home/pi/IoT-Rpi/%s.jpg' % timestamp, use_video_port=True)
+    print("fotografia tomada")
 
 button.when_pressed = capture
+      
 
 pause()
